c7addfb7654b2fcd5f96915c783fbbb8504135e5
Literate Snow <literatesnow@users.noreply.github.com>
2018-04-12 13:26:57 -0600

    Whitelist docker IP.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Web Console
+  config.web_console.whitelisted_ips = '172.17.0.0/12'
 end
+
